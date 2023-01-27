35d61afc941c311e25270e77e9f217dd4ed00dd4
Marcos Eduardo <bacalhau_1995@hotmail.com>
2023-01-27 13:52:48 -0300

    fix: remove column username

--- src/modules/accounts/dtos/ICreateUserDTO.ts
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -1,9 +1,8 @@
 interface ICreateUserDTO {
   name: string;
-  username: string;
   password: string;
   email: string;
   driver_license: string;
 }
- 
-export { ICreateUserDTO }
+
+export { ICreateUserDTO };
