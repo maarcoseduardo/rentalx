64e8f6f7c5eb5ac2ef28a0040aa34ecabbcebcf3
Marcos Eduardo <maarcosefb0@gmail.com>
2023-01-28 11:11:25 -0300

    chore: add jwt and resolve async-errors

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+import { AppError } from "../errors/AppError";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "373cf3fbb6132f2f6122ed8263a330af"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User does not exist!", 401);
+    }
+
+    next();
+  } catch {
+    throw new AppError("Invalid token!", 401);
+  }
+}
+
+export { ensureAuthenticated };
--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -30,6 +30,12 @@ class UsersRepository implements IUsersRepository {
 
     await this.repository.save(user);
   }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+
+    return user;
+  }
 }
 
 export { UsersRepository };
